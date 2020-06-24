ef1b6968a8e80d328ae39bfa5d200626d9b190cf
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-24 04:37:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,13 @@ def batch_badge_creator(names)
 end
 
 #rooms available 1-7 
-#
+#return  "Hello, _____! You'll be assigned to room _____!"
+
 def assign_rooms(names)
   
+end 
+
+def printer 
+  puts "${batch_badge_creator}"
+  puts "${assign_rooms}"
 end 

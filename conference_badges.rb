df3b539c49d4e6ab18aa710444d3ae408f2c6379
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-24 04:42:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 names = ["Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(names)
-"Hello, my name is ${names}"
+"Hello, my name is #{names}"
 end
 
 #rooms available 1-7 
@@ -17,6 +17,6 @@ def assign_rooms(names)
 end 
 
 def printer 
-  puts "${batch_badge_creator}"
-  puts "${assign_rooms}"
+  puts "#{batch_badge_creator}"
+  puts "#{assign_rooms}"
 end 
